c8a3da669ffcd32fda505e5873d841e85ac6a55d
Evan Bero <evanbero1980@gmail.com>
2020-08-09 15:01:02 -0400

    added api routes and js to add song

--- models/song.js
+++ models/song.js
@@ -1,8 +1,9 @@
 module.exports = function(sequelize, DataTypes){
+    // SEE IF THIS HAS TO MATCH THE DB TABLE NAMES IE: SONG_NAME
     var Song = sequelize.define("Song", {
-        artist: DataTypes.STRING,
-        songTitle: DataTypes.STRING,
-        album: DataTypes.STRING,
+        artistName: DataTypes.STRING,
+        songName: DataTypes.STRING,
+        albumName: DataTypes.STRING,
         mp3: DataTypes.STRING,
         albumArt: DataTypes.STRING,
     });
